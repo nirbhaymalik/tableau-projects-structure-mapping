58769318df6092b28b276241e0835aa3416aa89b
Nirbhay Malik <malik.nirbhay02@gmail.com>
2024-03-02 08:05:44 +1100

    phase 1 tree completed. - non-satisfactory results. data structures need to fixed.

--- convertor/__main__.py
+++ convertor/__main__.py
@@ -1,5 +1,11 @@
-pc = __import__('projects-convertor')
+pc = __import__('projects_convertor')
 
 projects_xml_tags = pc.extract_projects_from_xml()
+print(len(projects_xml_tags))
 projects_dict = pc.map_project_tags_to_dict(projects_xml_tags)
+print(len(projects_dict))
 projects_structure_tree = pc.create_project_tree_nodes_list(projects_dict)
+print(len(projects_structure_tree))
+
+for i in projects_structure_tree:
+    print(projects_structure_tree[i].__str__())
--- convertor/projects_convertor.py
+++ convertor/projects_convertor.py
@@ -5,37 +5,74 @@ import xml.etree.ElementTree as Xet
 from project import Project
 
 def extract_projects_from_xml():
-    xml_parse = Xet.parse('assets/project.xml')
+    xml_parse = Xet.parse('assets/projects.xml')
+
     ts_response = xml_parse.getroot()
+
     return ts_response[1]
+
 
 def map_project_tags_to_dict(projects_xml_tags):
     projects_dict = {}
+
     for project in projects_xml_tags:
         row_data = {}
+
         for item in project.items():
             row_data[item[0]] = item[1]
+
         projects_dict[project.attrib['id']] = row_data
+
     return projects_dict
 
-def add_project_with_parent(project_id, project_node, project_items, projects_as_structured_tree_nodes):
-    project_parent_id = project_items['parentProjectId']
-    if projects_as_structured_tree_nodes[project_parent_id] is not None:
-        project_parent_node = projects_as_structured_tree_nodes[project_parent_id]
-        project_node.set_parent(project_parent_node)
-        projects_as_structured_tree_nodes[project_id] = project_node
-    else:
-        pass
+
+def check_key_in_dict(key, dictionary) -> bool:
+    try:
+        dictionary[key]
+    except KeyError as key_error:
+        return False
+    return True
+
+
+def add_project_with_parent(project_id, items, projects_dict, projects_as_structured_tree_nodes):
+    parent_id = items['parentProjectId']
+    parent_items = projects_dict[parent_id]
+
+    if not check_key_in_dict('parentProjectId', parent_items):
+        project = Project(items['name'])
+
+        if projects_as_structured_tree_nodes[parent_id] is None:
+            parent_project = Project(parent_items['name'])
+            projects_as_structured_tree_nodes[parent_id] = parent_project
+        else:
+            parent_project = projects_as_structured_tree_nodes[parent_id]
+
+        project.set_parent(parent_project)
+        projects_as_structured_tree_nodes[project_id] = project
+
+        return
+    
+    add_project_with_parent(
+            parent_id, parent_items, projects_dict,
+            projects_as_structured_tree_nodes
+            )
+
 
 def create_project_tree_nodes_list(projects_dict):
     projects_as_structured_tree_nodes = {}
+
     for project_id in projects_dict:
-        if projects_as_structured_tree_nodes[project_id] is not None:
+        if check_key_in_dict(project_id, projects_as_structured_tree_nodes):
             continue
+
         project_items = projects_dict[project_id]
-        project_node = Project(project_items['name'])
-        
-        if project_items['parentProjectId'] is None:
+        if not check_key_in_dict('parentProjectId', project_items):
+            project_node = Project(project_items['name'])
             projects_as_structured_tree_nodes[project_id] = project_node
         else:
-            add_project_with_parent(project_id, project_node, project_items, projects_as_structured_tree_nodes)
+            add_project_with_parent(
+                project_id, project_items, projects_dict,
+                projects_as_structured_tree_nodes
+                )
+            
+    return projects_as_structured_tree_nodes
